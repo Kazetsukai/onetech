81a5b348c2d0be46abe3f8c043328f8868717f0b
Daniel Tebbutt <moeruze.burning@gmail.com>
2018-03-06 22:34:45 +1300

    Compiled sprites and gamedata

--- process/process.js
+++ process/process.js
@@ -1,15 +1,14 @@
 let exec = require('child_process').exec;
 let fs = require('fs');
-let fetch = require('node-fetch');
+let imagemagick = require('imagemagick');
 let _ = require('lodash');
 
 let baseDir = "OneLifeData7-master/"
-let gitRepo = "kazetsukai/"
 
 if (process.argv[2] === 'download') {
   console.log("Downloading data...");
 
-  let getData = exec('curl https://codeload.github.com/jasonrohrer/OneLifeData7/tar.gz/master | tar -xf -', (error, stdout, stderr) => {
+  let getData = exec('curl https://codeload.github.com/jasonrohrer/OneLifeData7/tar.gz/master | tar -xzf -', (error, stdout, stderr) => {
     console.log(error);
     console.log(stdout);
     console.log(stderr);
@@ -25,48 +24,68 @@ if (process.argv[2] === 'download') {
 function processData() {
   let objDir = baseDir + 'objects';
   let transDir = baseDir + 'transitions';
+  let spriteDir = baseDir + 'sprites';
 
+  // Load objects from files
   let objects = _.map(fs.readdirSync(objDir), f => {
-    console.log(f);
     let file = objDir + '/' + f;
-    return parseObject(fs.readFileSync(file, 'utf8'));
+    return parseObject(fs.readFileSync(file, 'utf8'), f);
   });
 
-  console.log("Pushing " + objects.length + " objects to GitHub...");
+  let objectArray = [];
+  for (let i in objects) {
+    let obj = objects[i];
+    if (obj.id)
+      objectArray[obj.id] = obj;
+  }
 
-  fetch("https://api.github.com/repos/kazetsukai/onetech/contents/src/assets/objects.json")
-    .then(response => response.json())
-    .then(response => {
-      let sha = response.sha;
-
-      postData("https://api.github.com/repos/kazetsukai/onetech/contents/src/assets/objects.json", {
-        message: "Updated object data",
-        content: Buffer.from(JSON.stringify(objects)).toString('base64'),
-        sha: sha
-      }).then(response => response.json())
-        .then(response => {
-          console.dir(response);
-        });
+  // Load transitions from files
+  let transitions = _.map(fs.readdirSync(transDir), f => {
+    let file = transDir + '/' + f;
+    return parseTransition(fs.readFileSync(file, 'utf8'), f);
   });
-  
+
+  // Convert images to png
+  console.log('Converting sprites...');
+  Promise.all(_.map(fs.readdirSync(spriteDir), f => {
+    if (!f.includes('tga'))
+      return "";
+
+    let inPath = spriteDir + '/' + f
+    let outPath = '../src/assets/sprites/' + f.split('.')[0] + '.png';
+
+    return new Promise((resolve, reject) => {
+      imagemagick.convert([inPath, outPath], (err, stdout) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  })).then(imgs => {
+    console.log('Converted ' + imgs.length + " sprites.");
+  }).catch(error => {
+    console.log(error);
+  });
+
+  //listTransitions(transitions, objectArray);
+
+  // Save final game data object
+  console.log("Saving gamedata blob...");
+  let gameData = {
+    objects: objects,
+    transitions: transitions
+  };
+
+  fs.writeFileSync('../src/assets/gamedata.json', JSON.stringify(gameData));
 }
 
-function postData (url, obj) {
-  return fetch(url, {
-    body: JSON.stringify(obj),
-    headers: {
-      'content-type': 'application/json',
-      'Authorization': 'token ' + process.env.ONETECH_GITHUB_TOKEN
-    },
-    method: 'PUT', 
-    redirect: 'follow',
-    referrer: 'no-referrer',
-  });
-}
-
-function parseObject (txt, url) {
+function parseObject (txt, file) {
   let lines = txt.split('\n');
-  let obj = { sprites: [] };
+  let obj = { 
+    sprites: []
+  };
 
   let i = 0;
 
@@ -114,4 +133,45 @@ function parseObject (txt, url) {
     obj.name = lines[1];
 
   return obj;
+}
+
+
+function parseTransition (txt, file) {
+  let trn = {};
+  let fileParts = file.split('.')[0].split('_');
+
+  trn.lastUseActor = (fileParts[2] === 'LA');
+  trn.lastUseTarget = (fileParts[2] === 'LT' || fileParts[2] === 'L');
+
+  trn.actorID = fileParts[0];
+  trn.targetID = fileParts[1];
+
+  let txtParts = txt.split(' ');
+
+  trn.newActorID = txtParts[0] || 0;
+  trn.newTargetID = txtParts[1] || 0;
+  trn.autoDecaySeconds = txtParts[2] || 0;
+  trn.actorMinUseFraction = txtParts[3] || 0;
+  trn.targetMinUseFraction = txtParts[4] || 0;
+  trn.reverseUseActor = !!txtParts[5];
+  trn.reverseUseTarget = !!txtParts[6];
+  trn.move = txtParts[7] || 0;
+  trn.desiredMoveDist = txtParts[8] || 1;
+
+  trn.epochAutoDecay = trn.autoDecaySeconds < 0 ? -trn.autoDecaySeconds : 0;
+
+  return trn;
+}
+
+function listTransitions (transitions, objectArray) {
+  for (let t in transitions) {
+    let trans = transitions[t];
+
+    let act = (objectArray[trans.actorID] || {}).name || "Nothing";
+    let trg = (objectArray[trans.targetID] || {}).name || "Nothing";
+    let newAct = (objectArray[trans.newActorID] || {}).name || "Nothing";
+    let newTrg = (objectArray[trans.newTargetID] || {}).name || "Nothing";
+
+    console.log(act + " + " + trg + " = " + newAct + " + " + newTrg);
+  }
 }
