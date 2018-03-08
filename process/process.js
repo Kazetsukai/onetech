e3db3bcf5b67dfd159cd7bae4ef197b35080478f
Daniel Tebbutt <daniel@shift72.com>
2018-03-08 23:19:10 +1300

    Yaharr! Somehow managed to maths my way through that one...

--- process/process.js
+++ process/process.js
@@ -1,11 +1,15 @@
 let exec = require('child_process').exec;
 let fs = require('fs');
 let imagemagick = require('imagemagick');
+let Canvas = require('canvas')
+let Image = Canvas.Image;
 let _ = require('lodash');
 
 let baseDir = "OneLifeData7-master/"
 
-if (process.argv[2] === 'download') {
+let download = process.argv[2] === 'download'
+
+if (download) {
   console.log("Downloading data...");
 
   let getData = exec('curl https://codeload.github.com/jasonrohrer/OneLifeData7/tar.gz/master | tar -xzf -', (error, stdout, stderr) => {
@@ -48,6 +52,7 @@ function processData() {
   // Convert images to png
   console.log('Converting sprites...');
   let files = fs.readdirSync(spriteDir);
+  let spriteDeets = [];
 
   let promise = Promise.resolve();
 
@@ -57,10 +62,22 @@ function processData() {
       if (!f.includes('tga'))
         return "";
 
+      let num = f.split('.')[0];
       let inPath = spriteDir + '/' + f
-      let outPath = '../static/sprites/' + f.split('.')[0] + '.png';
+      let outPath = '../static/sprites/sprite_' + num + '.png';
 
-      return new Promise((resolve, reject) => {
+      let bits = fs.readFileSync(spriteDir + '/' + num + '.txt', 'utf8').split(' ');
+      spriteDeets[num] = {
+        tag: bits[0],
+        multiplicativeBlending: bits[1] === '1',
+        centerAnchorXOffset: parseFloat(bits[2]),
+        centerAnchorYOffset: parseFloat(bits[3])
+      };
+
+      if (!download)
+        return Promise.resolve();
+      else
+        return new Promise((resolve, reject) => {
         imagemagick.convert([inPath, outPath], (err, stdout) => {
           if (err) {
             reject(err);
@@ -71,7 +88,91 @@ function processData() {
       });
     });
   }
-  promise.then(f => { console.log("Converted sprites"); })
+  promise.then(() => { 
+    console.log("Converted sprites, drawing objects");
+
+    let canvas = new Canvas(512, 1024);
+    let ctx = canvas.getContext('2d');
+
+    for (let o in objects) {
+      let obj = objects[o];
+      let sprites = obj.sprites;
+
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      let maxX = 0;
+      let maxY = 0;
+      let minX = 0;
+      let minY = 0;
+
+      for (let s in sprites) {
+        let sprite = sprites[s];
+        let ages = sprite.ageRange;
+        let targetAge = 20; // Draw sprites as if they were 20 years old
+        if ((ages[0] > -1 || ages[1] > -1) && (ages[0] > targetAge || ages[1] < targetAge)) continue;
+
+        let details = spriteDeets[sprite.id];
+
+        let img = new Image;
+        img.src = fs.readFileSync('../static/sprites/sprite_' + sprite.id + '.png');
+
+        let angleRads = parseFloat(sprite.rot) * Math.PI * 2;
+        let x = parseFloat(sprite.x);
+        let y = parseFloat(sprite.y);
+
+        let points = [
+          {x: -img.width/2, y: -img.height/2},
+          {x: img.width/2, y: -img.height/2},
+          {x: img.width/2, y: img.height/2},
+          {x: -img.width/2, y: img.height/2},
+        ]
+
+        let cosA = Math.cos(angleRads);
+        let sinA = Math.sin(angleRads);
+
+        for (let p in points) {
+          p = points[p];
+          p.x -= details.centerAnchorXOffset;
+          p.y -= details.centerAnchorYOffset;
+          p.x = p.x * cosA - p.y * sinA;
+          p.y = p.y * cosA + p.x * sinA;
+          p.x += x;
+          p.y += y;
+          if (p.x > maxX) maxX = p.x + 2;
+          if (p.x < minX) minX = p.x - 2;
+          if (p.y > maxY) maxY = p.y + 2;
+          if (p.y < minY) minY = p.y - 2;
+        }
+
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.translate(x + canvas.width / 2, -y + canvas.height / 2);
+        ctx.rotate(angleRads);
+        ctx.drawImage(img, -img.width / 2 - details.centerAnchorXOffset, -img.height / 2 - details.centerAnchorYOffset, img.width, img.height);
+      }
+
+      let tmpW = maxX - minX;
+      let tmpH = maxY - minY;
+
+      let newWidth = Math.max(tmpW, 128);
+      let newHeight = Math.max(tmpH, 128);
+
+      let tmpCanvas = new Canvas(newWidth, newHeight);
+      let tmpCtx = tmpCanvas.getContext('2d');
+
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+      tmpCtx.drawImage(canvas, minX + canvas.width / 2, -maxY + canvas.height / 2, tmpW, tmpH, (newWidth - tmpW) / 2, (newHeight - tmpH) / 2, tmpW, tmpH);
+
+      // Debug
+      ctx.beginPath(); //
+      ctx.rect(minX + canvas.width / 2, -maxY + canvas.height / 2, tmpW, tmpH);
+      ctx.stroke();
+      ctx.closePath(); //
+      
+      fs.writeFileSync('../static/sprites/obj_' + obj.id + '.png', tmpCanvas.toBuffer());
+    } 
+  })
 
   //listTransitions(transitions, objectArray);
 
@@ -119,6 +220,7 @@ function parseObject (txt, file) {
       sprite.x = pos[0];
       sprite.y = pos[1];
       sprite.rot = lines[i+2].split('=')[1];
+      sprite.ageRange = _.map(lines[i+5].split('=')[1].split(','), parseFloat);
 
       obj.sprites.push(sprite);
 
