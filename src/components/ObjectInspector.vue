f35171a6366a068fd5f7bc4954f0796136c67957
Ryan Bates <ryan@railscasts.com>
2018-03-19 16:30:14 -0700

    Adding insulation info to inspector

--- src/components/ObjectInspector.vue
+++ src/components/ObjectInspector.vue
@@ -14,6 +14,7 @@
         <ul>
           <li v-if="objectData.foodValue > 0">Food: {{objectData.foodValue}}</li>
           <li v-if="objectData.heatValue > 0">Heat: {{objectData.heatValue}}</li>
+          <li v-if="objectData.rValue > 0">Insulation: {{parseFloat(objectData.rValue)}}</li>
           <!-- <li v-if="objectData.complexity > 0">Complexity: {{objectData.complexity}}</li> -->
         </ul>
         <div class="techTree" v-if="objectData.techTree">
