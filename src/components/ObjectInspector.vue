f80fc116aa5172ac56cc76740c007303bfb9a6a7
Daniel Tebbutt <daniel@shift72.com>
2018-03-23 09:00:55 +1300

    Stop 'clothing' from flashing when changing item

--- src/components/ObjectInspector.vue
+++ src/components/ObjectInspector.vue
@@ -14,7 +14,7 @@
         <ul>
           <li v-if="objectData.foodValue > 0">Food: {{objectData.foodValue}}</li>
           <li v-if="objectData.heatValue > 0">Heat: {{objectData.heatValue}}</li>
-          <li v-if="objectData.clothing != 'n'">Clothing: {{clothingPart()}}</li>
+          <li v-if="objectData.clothing && objectData.clothing != 'n'">Clothing: {{clothingPart()}}</li>
           <li v-if="!isNaN(objectData.insulation)">Insulation: {{objectData.insulation.toFixed(4)*100}}%</li>
           <!-- <li v-if="objectData.complexity > 0">Complexity: {{objectData.complexity}}</li> -->
         </ul>
