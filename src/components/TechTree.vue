f71a61b4a22b7c223d96a0029976d20d80486cc4
Ryan Bates <ryan@railscasts.com>
2018-03-23 13:45:47 -0700

    Refactoring tech tree view

--- src/components/TechTree.vue
+++ src/components/TechTree.vue
@@ -3,66 +3,23 @@
     <h2>{{name}}</h2>
     <h3>Tech Tree</h3>
 
-    <h3 v-if="object.loading">Loading...</h3>
-
-    <div v-else class="tree">
-      <TechTreeNode
-        :objectID="objectID"
-        :nodes="object.techTree"
-        :selectedID="subtrees[0] && subtrees[0].id"
-        treeIndex="0"
-        @expand="expand"
-      />
-    </div>
-
-    <div class="tree subtree" v-for="(object, index) in subtrees" :key="index">
-      <TechTreeNode
-        :objectID="object.id"
-        :nodes="object.techTree"
-        :selectedID="subtrees[index+1] && subtrees[index+1].id"
-        :treeIndex="index+1"
-        @expand="expand"
-      />
-    </div>
+    <TechTreeView :objectID="objectID" />
   </div>
 </template>
 
 <script>
 import ObjectService from '../services/ObjectService'
 
-import TechTreeNode from './TechTreeNode';
+import TechTreeView from './TechTreeView';
 
 export default {
   props: ['objectID'],
   components: {
-    TechTreeNode
-  },
-  data () {
-    return {
-      object: null,
-      subtrees: []
-    };
-  },
-  beforeMount () {
-    this.loadObject();
+    TechTreeView
   },
   computed: {
     name () { return ObjectService.name(this.objectID); },
   },
-  watch: {
-    objectID () { this.loadObject(); }
-  },
-  methods: {
-    loadObject () {
-      this.object = {loading: true};
-      ObjectService.fetchObject(this.objectID, obj => this.object = obj);
-    },
-    expand (objectID, treeIndex) {
-      ObjectService.fetchObject(objectID, object => {
-        this.subtrees = this.subtrees.slice(0, treeIndex).concat([object]);
-      })
-    }
-  }
 }
 </script>
 
@@ -81,23 +38,10 @@ export default {
     margin-bottom: 0px;
   }
 
-  .techTree > h3 {
+  .techTree h3 {
     text-align: center;
     font-weight: lighter;
     font-style: italic;
     margin-top: 0px;
-  }
-
-  .tree {
-    text-align: center;
-    overflow-x: auto;
-    overflow-y: hidden;
-    box-sizing: border-box;
-  }
-
-  .subtree {
-    border-top: dashed 1px #777;
-    margin-top: 15px;
-    padding-top: 10px;
   }
 </style>
