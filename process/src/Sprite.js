4a6d238ecacf894e0fa809441732427b3990daaf
Ryan Bates <ryan@railscasts.com>
2019-04-06 12:14:13 -0700

    Changing how sprite data is parsed

--- process/src/Sprite.js
+++ process/src/Sprite.js
@@ -6,14 +6,50 @@ class Sprite {
   constructor(lines, index, object) {
     this.index = index;
     this.object = object;
-    this.id = lines[0].split('=')[1];
-    const position = lines[1].split('=')[1].split(',');
-    this.x = position[0];
-    this.y = position[1];
-    this.rotation = lines[2].split('=')[1];
-    this.hFlip = lines[3].split('=')[1];
-    this.color = lines[4].split('=')[1].split(',');
-    this.ageRange = lines[5].split('=')[1].split(',').map(f => parseFloat(f));
+    for (let line of lines) {
+      this.parseLine(line);
+    }
+  }
+
+  // Dupication with GameObject.js
+  parseLine(line) {
+    const assignments = line.split(/[,#]/);
+    let attribute = null;
+    let values = [];
+    for (let assignment of assignments) {
+      const parts = assignment.split(/[_=]/);
+      if (parts.length > 1) {
+        this.assignData(attribute, values);
+        attribute = parts.shift();
+        values = [];
+      }
+      values.push(this.parseValue(parts[0]));
+    }
+    this.assignData(attribute, values);
+  }
+
+  parseValue(value) {
+    if (isNaN(value))
+      return value;
+    if (value.includes("."))
+      return parseFloat(value);
+    return parseInt(value);
+  }
+
+  assignData(attribute, values) {
+    if (!attribute) return;
+    if (attribute === "spriteID") {
+      this.id = values[0].toString();
+    } else if (attribute === "pos") {
+      this.x = values[0];
+      this.y = values[1];
+    } else if (attribute === "rot") {
+      this.rotation = values[0];
+    } else if (values.length == 1) {
+      this[attribute] = values[0];
+    } else {
+      this[attribute] = values;
+    }
   }
 
   parseExtraData(data) {
